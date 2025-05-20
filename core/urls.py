d207769dc73486b24e37cb6851835fa9d1f2e730
jegger <jpedroegger@gmail.com>
2025-05-21 00:42:05 +0100

    refactor: change api url name

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/traffic/', include('traffic_monitor.api.urls')),
+    path('api/traffic/', include('traffic_monitor.api.urls')),
 ]
