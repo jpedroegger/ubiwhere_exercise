fff5b2a4cfc505b182ed1cc36901210199c5260b
jegger <jpedroegger@gmail.com>
2025-05-22 11:28:04 +0100

    fix: include a endpoint for swagger docs so dynamic docs would work properly

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('traffic_monitor.api.urls')),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
 ]
